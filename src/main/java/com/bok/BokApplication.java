6013d79563014c54d80742891cb0e4717a0a9e4e
jboob <tyyq9851@sian.com>
2018-09-13 17:40:09 +0800

    first submit！

---
+++ src/main/java/com/bok/BokApplication.java
@@ -0,0 +1,12 @@
+package com.bok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BokApplication.class, args);
+    }
+}
